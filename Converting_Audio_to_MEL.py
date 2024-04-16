70132402efbb951437a7d5344a5c5449d1439f22
DrorArbiv <dror1999@gmail.com>
2024-04-16 11:36:41 +0300

    converting from audio to mfcc in numpy file

--- Converting_Audio_to_MEL.py
+++ Converting_Audio_to_MEL.py
@@ -1,33 +1,31 @@
 import os
 import librosa
 import numpy as np
-import matplotlib.cm as cm
 
 ########## creating numpy files to the original files
 folder_path = './release_in_the_wild'
 nparray_files = './numpy_files'
 if not os.path.exists(nparray_files):
     os.mkdir(nparray_files)
+def extract_features(audio_path):
+    waveform, sample_rate = librosa.load(audio_path, sr=None)
+    mfccs = librosa.feature.mfcc(y=waveform, sr=sample_rate, n_mfcc=13)
+    ret = np.mean(mfccs, axis=1)
+    duration = librosa.get_duration(y= waveform, sr=sample_rate)
+    return ret, duration
+
 
 durations_origin = []
 for file_name in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file_name)
-    waveform, sample_rate = librosa.load(file_path)
-    duration = librosa.get_duration(y=waveform, sr=sample_rate)
+    mfcc, duration = extract_features(file_path)
     if duration < 1:
         continue
     durations_origin.append(duration)
-    mel_spectrogram = librosa.feature.melspectrogram(y=waveform, sr=sample_rate)
-    S_dB = librosa.power_to_db(mel_spectrogram, ref=np.min)
-    S_normalized = librosa.util.normalize(S_dB)
-    # Apply colormap to convert to RGB
-    spectrogram_rgb = cm.viridis(S_normalized)[:, :, :3]  # Keep only RGB channels
-    # Scale to 0-255 and convert to uint8
-    spectrogram_uint8 = (spectrogram_rgb * 255).astype(np.uint8)
     file_name_no_ext = os.path.splitext(file_name)[0]
-    file_path = os.path.join(nparray_files, file_name_no_ext + '.npy')
+    new_file_path = os.path.join(nparray_files, file_name_no_ext + '.npy')
     # Save the numpy array to the file
-    np.save(file_path, spectrogram_uint8)
+    np.save(new_file_path, mfcc)
 np.save(nparray_files + '_durations.npy', durations_origin)
 
 ########## creating numpy files to the cleaned and padded files
@@ -39,18 +37,8 @@ if not os.path.exists(nparray_files_clean):
 
 for file_name in os.listdir(folder_path_clean):
     file_path = os.path.join(folder_path_clean, file_name)
-    waveform, sample_rate = librosa.load(file_path)
-    duration = librosa.get_duration(y=waveform, sr=sample_rate)
-    if duration < 1:
-        continue
-    mel_spectrogram = librosa.feature.melspectrogram(y=waveform, sr=sample_rate)
-    S_dB = librosa.power_to_db(mel_spectrogram, ref=np.min)
-    S_normalized = librosa.util.normalize(S_dB)
-    # Apply colormap to convert to RGB
-    spectrogram_rgb = cm.viridis(S_normalized)[:, :, :3]  # Keep only RGB channels
-    # Scale to 0-255 and convert to uint8
-    spectrogram_uint8 = (spectrogram_rgb * 255).astype(np.uint8)
+    mfcc, _ = extract_features(file_path)
     file_name_no_ext = os.path.splitext(file_name)[0]
-    file_path = os.path.join(nparray_files_clean, file_name_no_ext + '.npy')
+    new_file_path = os.path.join(nparray_files_clean, file_name_no_ext + '.npy')
     # Save the numpy array to the file
-    np.save(file_path, spectrogram_uint8)
+    np.save(new_file_path, mfcc)
